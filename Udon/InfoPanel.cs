b9dc665deeb64b7c273e34acb2dc661674cc6c79
John K N Murphy <j.murpheous@gmail.com>
2023-11-23 03:48:14 +1300

    Update UI

--- Udon/InfoPanel.cs
+++ Udon/InfoPanel.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UdonSharp;
 using UnityEngine;
@@ -6,7 +8,6 @@ using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
-
 public class InfoPanel : UdonSharpBehaviour
 {
     public Toggle panelToggle;
@@ -15,6 +16,7 @@ public class InfoPanel : UdonSharpBehaviour
     [SerializeField] TextMeshProUGUI infoText;
 
     bool hasToggle = false;
+    bool hasGroup = false;
     bool hasTextField = false;
     bool hasTransform = false;
     [SerializeField] private bool showPanel = true;
@@ -34,11 +36,11 @@ public class InfoPanel : UdonSharpBehaviour
         {
             if (showPanel != value)
             {
-                showPanel = value;
                 if (value && hasTextField)
                 {
                     infoText.text = defaultText;
                 }
+                showPanel = value;
             }
             if (hasTextField)
                 infoText.enabled = showPanel;
@@ -46,10 +48,32 @@ public class InfoPanel : UdonSharpBehaviour
             { 
                 panelXfrm.gameObject.SetActive(showPanel);
             }
+            if (hasGroup && !showPanel)
+            {
+                subjectGroup.SetAllTogglesOff();
+            }
         }
+    }
+
+    public void onGroupSelect(string toggleName)
+    {
+        if (!hasGroup)
+            return;
+        IEnumerable<Toggle> togs = subjectGroup.ActiveToggles();
+        //foreach (Toggle tog in togs)
+        {
+           Debug.Log("Got Toggle [" + togs.ToString() + "]");
+        }
+    }
+    public void onPanelClose()
+    {
+        ShowPanel = false;
     }
     void Start()
     {
+        if (subjectGroup == null)
+            subjectGroup = gameObject.GetComponent<ToggleGroup>();
+
         hasToggle = panelToggle != null;
         hasTextField = infoText != null;
         if (hasTextField && panelXfrm == null)
---
+++ Udon/SyncedToggle.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SyncedToggle : UdonSharpBehaviour
+{
+    [SerializeField]
+    private Toggle toggle;
+    private bool hasToggle = false;
+    [SerializeField]
+    private UdonBehaviour toggleClient;
+    [SerializeField]
+    private string clientVariableName;
+    [SerializeField]
+    private string toggleName;
+
+
+    void Start()
+    {
+       if (toggle == null)
+            toggle = GetComponent<Toggle>();
+        hasToggle = toggle != null;
+        if (!hasToggle)
+            return;
+    }
+}
