686ec5ff5ea886e04c0d250d70df20969391aee2
John K N Murphy <j.murpheous@gmail.com>
2024-12-23 00:54:14 +1300

    Reorganized for demos

---
+++ UI/UdonScripts/UdonTransfer.cs
@@ -0,0 +1,70 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class UdonTransfer : UdonSharpBehaviour
+{ 
+    [SerializeField]
+    Transform[] objects;
+    [SerializeField]
+    Vector3[] targetPositions;
+
+    [SerializeField, FieldChangeCallback(nameof(Tween))]
+    float tween;
+
+    private float updatedValue = -1;
+    private int nTransforms = -1;
+    [SerializeField]
+    private Vector3[] startPoints;
+    [SerializeField]
+    private Vector3[] endPoints;
+
+    private void init()
+    {
+        nTransforms = objects != null ? objects.Length : 0;
+        if (nTransforms > 0)
+        {
+            startPoints = new Vector3[nTransforms];
+            for (int i = 0; i < nTransforms; i++)
+                startPoints[i] = objects[i] != null ? objects[i].position : transform.position;
+            endPoints = new Vector3[nTransforms];
+            int nOthers = targetPositions == null ? 0 : targetPositions.Length;
+            for (int i = 0; i < nTransforms; i++)
+            {
+                if (i < nOthers && targetPositions[i] != null)
+                    endPoints[i] = targetPositions[i];
+                else
+                    endPoints[i] = startPoints[i];
+            }
+            updatedValue = -1;
+            Tween = tween;
+        }
+    }
+    public float Tween 
+    {   
+        get => tween;
+        set 
+        {
+            tween = Mathf.Clamp01(value);
+            if (updatedValue != tween)
+            {
+                for (int i = 0; i < nTransforms; i++)
+                {
+                    if (objects[i] != null)
+                    {
+                        objects[i].position = Vector3.Lerp(startPoints[i], endPoints[i], tween);
+                    }
+                }
+                updatedValue = tween;
+            }
+        }
+    }
+
+    void Start()
+    {
+        init();
+    }
+}
